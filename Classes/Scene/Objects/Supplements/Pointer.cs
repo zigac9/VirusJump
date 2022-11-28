93c8250e42e96e3eaefb8d3d84e852b478a56928
zigac9 <62549216+zigac9@users.noreply.github.com>
2022-11-28 18:24:13 +0100

    B2  configuring supplements classes

--- Classes/Scene/Objects/Supplements/Pointer.cs
+++ Classes/Scene/Objects/Supplements/Pointer.cs
@@ -3,12 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace VirusJump.Classes.Scene.Objects.Supplements
 {
     public class Pointer
     {
+        private Rectangle _position;
+        private Texture2D _texture;
 
         public Pointer() { }
+        
+        public void Draw(SpriteBatch s)
+        {
+            s.Draw(_texture, _position, Color.White);
+        }
     }
 }
--- Classes/Scene/Objects/Supplements/Scoring.cs
+++ Classes/Scene/Objects/Supplements/Scoring.cs
@@ -3,11 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace VirusJump.Classes.Scene.Objects.Supplements
 {
     public class Scoring
     {
+        private int _s;
+        private string _bestS;
+        private Vector2 _position;
+        private SpriteFont _spFont;
+        private bool _check;
+
         public Scoring() { }
+
+        public void Draw(SpriteBatch sp, int game)
+        {
+            if (game != 0 && game != 3 && game != 5)
+                sp.DrawString(_spFont, _s.ToString(), _position, Color.White);
+        }
     }
 }
